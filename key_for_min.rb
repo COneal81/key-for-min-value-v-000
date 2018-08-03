3d83b842ed6918375809dee062ae16736f30d81c
Kari O'Neal <triadfirewood@gmail.com>
2018-08-03 21:28:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_amount = " "
+ 
   name_hash.detect do |furniture, cost|
     if cost <= 50
       min_amount = furniture
